6ced0c458c36460c1d023d11bf5a39c29968a4ca
Alix <akshayalix12@gmail.com>
2023-05-29 22:19:02 +0530

    added all rules

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ def remove_rule():
     rule = "netsh advfirewall firewall delete rule name='BadIP'"
     subprocess.run(["Powershell", "-Command", rule])
 
+
 # Adding Inbound Rule 
+@eel.expose
 def inbound_rule():
     response = requests.get("https://feodotracker.abuse.ch/downloads/ipblocklist.csv").text
 
@@ -25,8 +27,23 @@ def inbound_rule():
             rule = "netsh advfirewall firewall add rule name='BadIP' Dir=In Action=Block RemoteIP=" + ip  # Fill Dir= with (Out or In)
             subprocess.run(["Powershell", "-Command", rule])
 
+# Adding Outbound Rule 
+@eel.expose
+def outbound_rule():
+    response = requests.get("https://feodotracker.abuse.ch/downloads/ipblocklist.csv").text
+
+    rule = "netsh advfirewall firewall delete rule name= 'BadIP'"
+    subprocess.run(["Powershell", "-Command", rule])
+
+    mycsv = csv.reader(filter(lambda x: not x.startswith("#"), response.splitlines()))
+    for row in mycsv:
+        ip = row[1]
+        if(ip)!=("dst_ip"):
+            print("Added Rule to block:", ip)
+            rule = "netsh advfirewall firewall add rule name='BadIP' Dir=Out Action=Block RemoteIP=" + ip  # Fill Dir= with (Out or In)
+            subprocess.run(["Powershell", "-Command", rule])
 
 
 
 
-eel.start('index.html', size=(800, 600), position=(750, 300))
+eel.start('index.html', size=(700, 500), position=(750, 300))
