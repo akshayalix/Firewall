a66293d8f2639079021f40c5c4a5b8c273d0698e
Alix <akshayalix12@gmail.com>
2023-10-11 20:33:13 +0530

    fix start script

--- start.bat
+++ start.bat
@@ -1,4 +1,18 @@
-set PYTHONHOME=%cd%
+@echo off
+setlocal
 
-python -m venv .venv
+set "Current=%~dp0"
+
+:: Activate the virtual environment
+call "%Current%.venv\Scripts\activate.bat"
+
+:: Start your Eel application without displaying a console window
+start %Current%.venv\Scripts\python.exe "%Current%app.py"
+
+:: Deactivate the virtual environment
+deactivate
+
+pause
+
+
 
