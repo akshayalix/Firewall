de76c0592389836380da7431f9dc28717299b55b
Alix <akshayalix12@gmail.com>
2024-02-07 19:59:00 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,13 +1,15 @@
 @echo off
 setlocal
 
+:: Set the current directory
 set "Current=%~dp0"
 
 :: Activate the virtual environment
 call "%Current%.venv\Scripts\activate.bat"
 
 :: Start your Eel application without displaying a console window
-start %Current%.venv\Scripts\python.exe "%Current%app.py"
+start "" "%Current%.venv\Scripts\python.exe" "%Current%app.py"
+
 
 
 
