6007ed0f2e50a39157b95c284d8a6c359f52cc22
Alix <57768355+akshayalix@users.noreply.github.com>
2024-02-07 20:05:53 +0530

    fixes & cleanup (#3)
    * updated
    
    * init
    
    * testing in-browser fun.
    
    * testing console output
    
    * testing run-as-admin
    
    * console output working with -chrome launch
    
    * fix start script
    
    * fixes + output console working.
    
    * switching to chrome-app ver
    
    * some testing
    
    * fixed window mode
    
    * tests
    
    * Update requirements.txt
    
    * dump idea
    
    * fixes
    
    * Update start.bat
    
    * Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,16 @@
-set PYTHONHOME=%cd%
+@echo off
+setlocal
 
-python -m venv .venv
+:: Set the current directory
+set "Current=%~dp0"
+
+:: Activate the virtual environment
+call "%Current%.venv\Scripts\activate.bat"
+
+:: Start your Eel application without displaying a console window
+start "" "%Current%.venv\Scripts\python.exe" "%Current%app.py"
+
+
+
+
 
